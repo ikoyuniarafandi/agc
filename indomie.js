08e4c51b8755a0e4a60c148a522892a2c30bc733
ikoyuniarafandi <ikoyuniar@gmail.com>
2021-06-13 14:01:41 +0700

    Create indomie.js

---
+++ indomie.js
@@ -0,0 +1,38 @@
+$(document).ready(function()
+{
+    console.log('jquery ready!');
+
+	var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ads fb 1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-6974290213233116"
+     data-ad-slot="3778618165"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<button class='g_url btn btn-success btn-dwn m-2'>Confirm</button> <br/></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
+
+	$(document.body).append(popbox);
+
+    if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s)))
+    {
+		$(window).scroll(function (event) {
+		    var scroll = $(window).scrollTop();
+		    if (scroll >= 200) {
+		        $('#popbox').removeClass('hide');
+		    }
+		    console.log('scroll..');                    
+		});
+    }
+
+    $(document).on('click','.g_url',function(e)
+    {
+        e.preventDefault();            
+
+        window.open(direct_link_ads,"_blank");
+        window.location.href = go_current;
+    });
+
+});
